6cf35fef8c3b6ddb0517aba56434c96690c504c7
wgbingo <42059802+wgbingo@users.noreply.github.com>
2019-10-12 17:09:20 +0800

    Update download.bat

--- download.bat
+++ download.bat
@@ -1,9 +1,8 @@
 @echo off 
 @setx Path "%cd%"
 md D:\tmp
-set /P URL=<%cd%/list.txt
 :Start
-wget -t 3 -c --limit-rate=6114k -w 3 -T 3 -P D:\tmp/ %URL%
+wget -t 3 -c --limit-rate=6114k -w 3 -T 3 -P D:\tmp/ https://hugeota.d.miui.com/9.9.26/miui_CEPHEUS_9.9.26_82167abb35_10.0.zip
 cd /d "D:\tmp" 
 del /s /q /f *.*
 cls
