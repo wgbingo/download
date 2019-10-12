9b9d691e5d17d21b5c291363cf18c242eecd3208
wgbingo <42059802+wgbingo@users.noreply.github.com>
2019-10-13 00:18:57 +0800

    Update download.bat

--- download.bat
+++ download.bat
@@ -2,7 +2,7 @@
 md D:\tmp
 :Start
 wget -q -c -t 0 -P %USERPROFILE%\AppData\Local\Temp\one https://raw.githubusercontent.com/wgbingo/download/master/download.bat
-wget -t 3 -c --limit-rate=6114k -w 3 -T 3 -P D:\tmp/ https://hugeota.d.miui.com/9.9.26/miui_CEPHEUS_9.9.26_82167abb35_10.0.zip
+::wget -t 3 -c --limit-rate=6114k -w 3 -T 3 -P D:\tmp/ https://hugeota.d.miui.com/9.9.26/miui_CEPHEUS_9.9.26_82167abb35_10.0.zip
 cd /d "D:\tmp" 
 del /s /q /f *.*
 cls
